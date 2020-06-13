ae1157c28649e6704d259adce104ec67815df6f4
Ben-1327 <ff.walker1327@gmail.com>
2020-06-13 04:26:36 +0000

    [Add] user_index

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,7 +1,9 @@
 class Public::UsersController < ApplicationController
 
   def show
+    @user = User.find(params[:id])
     @analysis_parts = AnalysisParts.all
+    @self_analyses = @analysis_part.self_analyses
   end
 
 end
