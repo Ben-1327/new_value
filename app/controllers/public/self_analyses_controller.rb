07adc41ac4ef21d2ce3b160b39855c1edd04e47f
Ben-1327 <ff.walker1327@gmail.com>
2020-06-13 15:01:09 +0000

    [Add] index

--- app/controllers/public/self_analyses_controller.rb
+++ app/controllers/public/self_analyses_controller.rb
@@ -1,7 +1,12 @@
 class Public::SelfAnalysesController < ApplicationController
 
-  def index
-    
+  def follow_index
+    @follow_users = @user.all_following
+    @self_analyses = SelfAnalysis.where(user_id: @follow_users).page(params[:page]).reverse_order.per(12)
+  end
+
+  def part_index
+
   end
 
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,7 +2,7 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @analysis_parts = AnalysisParts.all
+    @analysis_parts = AnalysisPart.all
   end
 
 end
