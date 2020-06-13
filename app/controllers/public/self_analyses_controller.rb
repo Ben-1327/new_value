2efbf256b22a864055eef5323e5f2747bee8439d
Ben-1327 <ff.walker1327@gmail.com>
2020-06-14 04:50:37 +0900

    [Add] some self_analysis views

--- app/controllers/public/self_analyses_controller.rb
+++ app/controllers/public/self_analyses_controller.rb
@@ -1,12 +1,33 @@
 class Public::SelfAnalysesController < ApplicationController
 
+  # フォロー中のユーザーの投稿
   def follow_index
     @follow_users = @user.all_following
     @self_analyses = SelfAnalysis.where(user_id: @follow_users).page(params[:page]).reverse_order.per(12)
   end
 
-  def part_index
+  # 各ユーザーの投稿
+  def user_index
+    @user = User.find(params[:id])
+    @self_analyses = @user.self_analyses.page(params[:page]).reverse_order.per(12)
+  end
 
+  # すべてのユーザーの投稿
+  def all_index
+    @self_analyses = SelfAnalysis.page(params[:page]).reverse_order.per(12)
+  end
+
+  # 各編の投稿
+  def user_part_index
+    @user = User.fond(params[:id])
+    @analysis_part = AnalysisPart.find(params[:id])
+    @self_analyses = @analysis_part.self_analyses.all
+  end
+
+  def show
+    @comment = SelfAnalysisComment.new
+    @self_analysis = SelfAnalysis.find(params[:id])
+    @analysis_part = @self_analysis.analysis_part
   end
 
 end
