f1078234cb158ce8892d8b058e16b800b67630f1
Ben-1327 <ff.walker1327@gmail.com>
2020-06-16 14:45:33 +0900

    [Add] favorite system

--- app/models/self_analysis.rb
+++ app/models/self_analysis.rb
@@ -1,8 +1,13 @@
 class SelfAnalysis < ApplicationRecord
 
   has_many :self_analysis_comments,dependent: :destroy
+  has_many :favorites,dependent: :destroy
 
   belongs_to :user
   belongs_to :user_question
+
+  def favorited_by?
+    favorites.where(user_id: user.id).exists?
+  end
 
 end
