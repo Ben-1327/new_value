b27c3aed9bc6aa932826d4937a120372552e01cc
Ben-1327 <ff.walker1327@gmail.com>
2020-06-19 17:55:53 +0900

    [Fix] routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,22 +17,39 @@ root 'public/homes#top'
 namespace :public do
   get 'about' => 'homes#about'
   get 'rule' => 'homes#rule'
+  get 'all_index' => 'self_analyses#all_index'
+  get 'follow_index' => 'self_analyses#follow_index'
+  get 'search' => 'searches#search'
 
 
-  resources :users, only: [:show, :edit, :update, :index] do
-  	resource :follows, only: [:create, :destroy, :follow_list, :follower_list]
+  resources :users, only: [:index, :show, :edit, :update, :index] do
     # フォローとアンフォロー機能
     put 'follow' => 'follows#follow'
     put 'unfollow' => 'follows#unfollow'
-
     # フォロー・フォロワーの一覧ページ
     get 'follow_list' => 'follows#follow_list'
     get 'follower_list' => 'follows#follower_list'
+
+    get 'user_part_index/:analysis_part_id' => 'self_analyses#follow_index'
+
+    resources :self_analyses, only: [:show, :edit, :update, :new, :destroy] do
+      resources :self_analysis_comments, only: [:create, :destroy]
+      resource :favorites, only: [:create,:destroy]
+    end
+
+    resource :follows, only: [:create, :destroy, :follow_list, :follower_list]
   end
 
-  resources :self_analyses, only: [:show, :edit, :update, :new, :destroy] do
-    resources :self_analysis_comments, only: [:create, :destroy]
+
+
+  resources :companies, only: [:show] do
+    # フォローとアンフォロー機能
+    put 'follow' => 'follows#follow'
+    put 'unfollow' => 'follows#unfollow'
+    # フォローの一覧ページ
+    get 'follow_list' => 'follows#follow_list'
   end
+
 
   resources :messages, only: [:create]
   resources :rooms, only: [:create,:show]
