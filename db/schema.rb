bdcb6974a3747f10aa223cd319bcbc3b3f7f009f
Ben-1327 <ff.walker1327@gmail.com>
2020-07-13 22:50:08 +0900

    200713

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_064005) do
+ActiveRecord::Schema.define(version: 2020_07_13_065109) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -153,8 +153,8 @@ ActiveRecord::Schema.define(version: 2020_07_13_064005) do
     t.integer "tag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "hp"
     t.integer "user_type", default: 0, null: false
+    t.integer "company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
