9af0e36e99034db3e35b79053dc38335db5aedd5
Ben-1327 <ff.walker1327@gmail.com>
2020-07-18 17:34:24 +0900

    [Fix] company view

--- app/controllers/public/companies_controller.rb
+++ app/controllers/public/companies_controller.rb
@@ -22,7 +22,7 @@ class Public::CompaniesController < ApplicationController
       if @company.save
         current_user.company_id = @company.id
         current_user.save
-    		redirect_to public_user_path(current_user), notice: "アカウント連携に成功しました!"#保存された場合の移動先を指定.
+    		redirect_to public_user_path(current_user), notice: "会社登録に成功しました!"#保存された場合の移動先を指定.
       else
     		render :new
       end
@@ -42,7 +42,7 @@ class Public::CompaniesController < ApplicationController
     elsif user_signed_in? && current_user.user_type == 1 && current_user.company_id.nil?
       render :new
     elsif user_signed_in? && current_user.user_type == 0
-      redirect_to public_user_path(current_user), notice: "あなたはビジネスアカウントでないので会社登録はできません"#保存された場合の移動先を指定.
+      redirect_to public_user_path(current_user), notice: "あなたはビジネスアカウントでないので会社編集はできません"#保存された場合の移動先を指定.
     else
       render 'home/top'
     end
@@ -52,14 +52,32 @@ class Public::CompaniesController < ApplicationController
     if user_signed_in? && current_user.user_type == 1 && current_user.company_id.present?
       @company = Company.find(params[:id])
       if @company.update(company_params)
-    		redirect_to public_user_path(current_user), notice: "アカウント連携に成功しました!"#保存された場合の移動先を指定.
+    		redirect_to public_user_path(current_user), notice: "会社編集に成功しました!"#保存された場合の移動先を指定.
       else
     		render :edit
       end
     elsif user_signed_in? && current_user.user_type == 1 && current_user.company_id.nil?
       render :new
     elsif user_signed_in? && current_user.user_type == 0
-      redirect_to public_user_path(current_user), notice: "あなたはビジネスアカウントでないので会社登録はできません"#保存された場合の移動先を指定.
+      redirect_to public_user_path(current_user), notice: "あなたはビジネスアカウントでないので会社編集はできません"#保存された場合の移動先を指定.
+    else
+      render 'home/top'
+    end
+  end
+
+  def destroy
+    if user_signed_in? && current_user.user_type == 1 && current_user.company_id.present?
+      @company = Company.find(params[:id])
+      if @company.destroy(company_params)
+        current_user.update(company_id: nil)
+    		redirect_to public_user_path(current_user), notice: "会社削除に成功しました!"#保存された場合の移動先を指定.
+      else
+    		render :edit
+      end
+    elsif user_signed_in? && current_user.user_type == 1 && current_user.company_id.nil?
+      render :new
+    elsif user_signed_in? && current_user.user_type == 0
+      redirect_to public_user_path(current_user), notice: "あなたはビジネスアカウントでないので会社削除はできません"#保存された場合の移動先を指定.
     else
       render 'home/top'
     end
