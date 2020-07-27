e1f3f04a6140a6670aa8d306c01cd1e42fb9e4db
Ben-1327 <ff.walker1327@gmail.com>
2020-07-27 22:10:20 +0900

    [Fix] config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
+  config.public_file_server.enabled = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
@@ -91,4 +92,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
 end
